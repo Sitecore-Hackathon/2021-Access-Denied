84ac7361f1e48a3f3881fad60e9a8d3e1b1407f9
ksorthiya <ksorthiya@horizontal.com>
2021-03-06 18:48:40 +0530

    Updated Solution and Projects

---
+++ src/Feature/MediaAuditStats/code/Models/ImageItemNoAltText.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Feature.MediaAuditStats.Models
+{
+    public class ImageItemNoAltText
+    {
+        public string Size { get; set; }
+
+        public string Extension { get; set; }
+
+        public string Updated { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public string Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string Path { get; set; }
+    }
+}
